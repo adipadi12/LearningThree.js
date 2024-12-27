40ee99d5c5d59bd714968fe70b2c884fc1f1bdaa
adipadi12 <ajha4436@gmail.com>
2024-12-27 06:46:06 +0530

    React Three Fiber and drei kinda ez

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,8 +1,42 @@
-const app = () => {
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { useRef } from 'react'
+ //direct equivalent of new THREE.Mesh is mesh
+const RotatingCube = () => {
+  const meshRef = useRef();
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += 0.01;
+      meshRef.current.rotation.y += 0.01;
+    }
+  });
+
   return (
-    <div>
-      <h1>hello Three.js</h1>
-    </div>
+    <mesh ref = {meshRef}>
+      <cylinderGeometry args={[1, 1, 1]} />
+      <meshLambertMaterial color="#468585" emissive='#468585' />
+    </mesh>
+  );
+};
+const App = () => {
+  return (
+    <Canvas 
+    style ={{
+      width: '100vw',
+      height: '100vh', 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center'
+      }}
+      >
+        <OrbitControls enableZoom={true} enablePan enableRotate/>
+
+        <directionalLight position={[1, 1, 1]} intensity={10} color = {'0x9CDBA6'} />
+
+        <color attach={'background'} args={['#F0F0F0']} />
+
+        <RotatingCube />
+    </Canvas>
   )
 }
-export default app
+export default App
