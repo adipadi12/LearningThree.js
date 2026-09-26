04ef3392b724e53a8366820e65d80ba2483e2a4a
Aditya Jha <ajha4436@gmail.com>
2026-09-26 20:45:18 +0530

    completed 3d text

--- 3Dtext/12-3d-text/src/script.js
+++ 3Dtext/12-3d-text/src/script.js
@@ -1,6 +1,9 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
+import typefaceFont from '../static/fonts/helvetiker_regular.typeface.json'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
 
 /**
  * Base
@@ -19,6 +22,9 @@ const scene = new THREE.Scene()
  */
 const textureLoader = new THREE.TextureLoader()
 
+const matcapTexture = textureLoader.load('/textures/matcaps/1.png')
+matcapTexture.colorSpace = THREE.SRGBColorSpace
+
 /**
  * Object
  */
@@ -27,7 +33,61 @@ const cube = new THREE.Mesh(
     new THREE.MeshBasicMaterial()
 )
 
-scene.add(cube)
+/**
+ * Fonts
+ */
+const fontLoader = new FontLoader()
+
+fontLoader.load(
+    '/fonts/helvetiker_regular.typeface.json',
+    (font) =>
+    {
+        const textGeometry = new TextGeometry(
+            'Hello Three.js',
+            {
+                font: font,
+                size: 0.5,
+                depth: 0.2,
+                curveSegments: 12,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                bevelSize: 0.02,
+                bevelOffset: 0,
+                bevelSegments: 5
+            }
+        )
+        const textMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+        const text = new THREE.Mesh(textGeometry, textMaterial)
+        textGeometry.computeBoundingBox()
+        console.log(textGeometry.boundingBox)
+
+        // textGeometry.translate(
+        //     - textGeometry.boundingBox.max.x * 0.5,
+        //     - textGeometry.boundingBox.max.y * 0.5,
+        //     - textGeometry.boundingBox.max.z * 0.5
+        // )
+        textGeometry.center()
+
+        scene.add(text)
+    }
+)
+
+const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+const donutMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+
+for(let i = 0; i < 100; i++)
+{
+    const donut = new THREE.Mesh(donutGeometry, donutMaterial)
+    donut.position.x = (Math.random() - 0.5) * 10
+    donut.position.y = (Math.random() - 0.5) * 10
+    donut.position.z = (Math.random() - 0.5) * 10
+    donut.rotation.x = Math.random() * Math.PI
+    donut.rotation.y = Math.random() * Math.PI
+    const scale = Math.random()
+    donut.scale.set(scale, scale, scale)
+    scene.add(donut)
+}
+//scene.add(cube)
 
 /**
  * Sizes
